ed1cb24eaf83c04138c30aaf6de4f10aba560248
mabiasio <mabiasio@gmail.com>
2018-05-10 12:55:22 +0200

    Added More Modules Libraries

--- Test_Sequences/debug.py
+++ Test_Sequences/debug.py
@@ -7,11 +7,19 @@ module=qsfp28()
 
 
 
+#module.CDR_disable()
+
+#time.sleep(2)
+
+#module.CDR_enable()
+
 print module.get_serial_number()
 print module.get_voltage()
 print module.get_temperature()
 
 print module.get_RX_power()
+
+print module.poller()
 
 #module.CDR_enable()
 
